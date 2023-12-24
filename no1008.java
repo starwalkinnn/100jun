91ff543f1dae9ad3901f38634e99cbfee003ce28
starwalkinnn <136154784+starwalkinnn@users.noreply.github.com>
2023-12-24 23:53:02 +0900

    나눗셈

---
+++ no1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class no1008{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double a, b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = a/b;
+		System.out.println(c);
+	}
+}
