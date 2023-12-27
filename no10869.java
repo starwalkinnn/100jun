2265d2518cf1566295f9f6198dc979893709935f
starwalkinnn <136154784+starwalkinnn@users.noreply.github.com>
2023-12-27 18:49:44 +0900

    사칙연산

---
+++ no10869.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class no10869{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a, b,c,d,e,f,g;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = a+b;
+        d = a-b;
+        e = a*b;
+        f = a/b;
+        g = a%b;
+		System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+		sc.close();
+	}
+}
+
